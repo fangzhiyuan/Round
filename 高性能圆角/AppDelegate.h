02ea2119501019064865779309bb8f82df428c63
fzy <fangzhiyuan@51pain.com>
2018-03-15 17:24:23 +0800

    Initial Commit

---
+++ 高性能圆角/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  高性能圆角
+//
+//  Created by fzy on 2018/3/15.
+//  Copyright © 2018年 fzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
