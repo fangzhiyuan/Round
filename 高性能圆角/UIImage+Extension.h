ee8c7dd86c6f2b1b251699e0fb718be425425ce8
fzy <fangzhiyuan@51pain.com>
2018-04-02 15:52:06 +0800

    图片圆角

---
+++ 高性能圆角/UIImage+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extension.h
+//  高性能圆角
+//
+//  Created by fzy on 2018/3/15.
+//  Copyright © 2018年 fzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+
+@end
---
+++ 高性能圆角/UIImage+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extension.m
+//  高性能圆角
+//
+//  Created by fzy on 2018/3/15.
+//  Copyright © 2018年 fzy. All rights reserved.
+//
+
+#import "UIImage+Extension.h"
+
+@implementation UIImage (Extension)
+
+@end
